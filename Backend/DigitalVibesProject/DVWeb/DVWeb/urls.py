75d7c13c301474e66f3a1a9f5e4722656732d829
UlyssesLopes <ulysses.rlolopes@gmail.com>
2023-01-17 09:04:19 -0300

    Integraçao com arquivos estáticos, configuraçao de templates e criaçao de endpoints vinculados a functions

--- Backend/DigitalVibesProject/DVWeb/DVWeb/urls.py
+++ Backend/DigitalVibesProject/DVWeb/DVWeb/urls.py
@@ -1,22 +1,15 @@
-"""DVWeb URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
+from pagina import views
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('qr-code/', views.paginaQRCode),
+    path('qr-code/message/', views.random_message),
+    path('qr-code/message/new/', views.new_message),
 
 ]
--- Backend/DigitalVibesProject/DVWeb/pagina/views.py
+++ Backend/DigitalVibesProject/DVWeb/pagina/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+
+def paginaQRCode(request):
+    return render(request, 'index.html')
+
+
+def random_message(request):
+    return render(request, 'message.html')
+
+
+def new_message(request):
+    return render(request, 'new-message.html')
+
+def save_form(request):
+    nome = request.POST.get('nome')
+
+    data = {
+        'nome': nome
+    }
+
+
+
+    return render(request, 'thanks.html', data)
